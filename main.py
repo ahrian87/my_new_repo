f3b636bd000c48771f1e242868689ccb1a94af85
ahrian87 <maciej.zbydniewski@gmail.com>
2021-10-14 20:32:41 +0200

    I changed hi to hello and add a new question

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 def print_hi(name):
-    print(f'Hi, {name}')
+    print(f'Hello, {name}')
 
 
 if __name__ == '__main__':
     print_hi('Maciej')
-    print("How are you?")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print("How are you doing?")
+    print("Where do you live?")
