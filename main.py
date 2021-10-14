4c058662dad49cf605655a8336dca71f040013e7
ahrian87 <maciej.zbydniewski@gmail.com>
2021-10-14 20:49:37 +0200

    testing new function

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ def print_hi(name):
     print(f'Hello, {name}')
 
 
+def my_function(a, b):
+    return a + b
+
+
 if __name__ == '__main__':
     print_hi('Maciej')
 
     print("How are you doing?")
     print("Where do you live?")
 
+    print(my_function(2, 4))
