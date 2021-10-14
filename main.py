e68d562d3274c2b105da4c0e9a353aa220c1192e
ahrian87 <maciej.zbydniewski@gmail.com>
2021-10-14 20:29:50 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+if __name__ == '__main__':
+    print_hi('Maciej')
+    print("How are you?")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
